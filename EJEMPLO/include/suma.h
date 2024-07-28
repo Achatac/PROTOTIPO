ccaac852a79e9111cfe00f170c3569bb8b787496
Achatac <achatac@unjbg.edu.pe>
2024-07-28 15:04:43 -0500

    ejemplo

---
+++ EJEMPLO/include/suma.h
@@ -0,0 +1,8 @@
+//suma.cpp
+
+#ifndef SUMA_H
+#define SUMA_H
+
+void suma();
+
+#endif // SUMA_H
---
+++ EJEMPLO/src/main.cpp
@@ -0,0 +1,12 @@
+//main.cpp 
+#include <iostream>
+#include "suma.h"
+
+using namespace std;
+
+int main (){
+    cout << "suma de numeros" << endl;
+
+    suma();
+    return 0;
+}
